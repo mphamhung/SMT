6c8658df9a268a186471b146d00249020dec0923
Michael <mphamhung@gmail.com>
2019-02-27 20:16:41 -0500

    test

--- code/test.py
+++ code/test.py
@@ -1,8 +1,9 @@
 from preprocess import *
 from lm_train import *
+from log_prob import *
 
 import os
-s = 'That process is underway No. Col. '
+s = 'That process is underway'
 
 data_dir = os.getcwd() + '/../data/'
 # for data in os.listdir(data_dir):
@@ -11,6 +12,15 @@ data_dir = os.getcwd() + '/../data/'
 #             print(sent)
 #             print(preprocess(sent, 'f'))
 
-lm_train(data_dir, 'e', 'test')
-lm_train(data_dir, 'f', 'test')
+# lm_train(data_dir, 'e', 'test')
+# lm_train(data_dir, 'f', 'test')
 # print(preprocess(s, 'e'))
+if os.path.getsize(os.getcwd()+'/etest.pickle') > 0:      
+    with open(target, "rb") as f:
+        unpickler = pickle.Unpickler(f)
+        # if file is not empty scores will be equal
+        # to the value unpickled
+        lm1 = unpickler.load()
+
+
+log_prob(s, lm1)
