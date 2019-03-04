57cf28b91d58125c7a2f27ec5f13f44c05495213
Michael <mphamhung@gmail.com>
2019-03-04 16:32:35 -0500

    fixed indentsin

--- code/align_ibm1.py
+++ code/align_ibm1.py
@@ -11,31 +11,31 @@ def align_ibm1(train_dir, num_sentences, max_iter, fn_AM):
 
         INPUTS:
         train_dir : 	(string) The top-level directory name containing data
-					e.g., '/u/cs401/A2_SMT/data/Hansard/Testing/'
+                    e.g., '/u/cs401/A2_SMT/data/Hansard/Testing/'
         num_sentences : (int) the maximum number of training sentences to consider
-	max_iter : 		(int) the maximum number of iterations of the EM algorithm
-	fn_AM : 		(string) the location to save the alignment model
-	
-	OUTPUT:
-	AM :			(dictionary) alignment model structure
-	
-	The dictionary AM is a dictionary of dictionaries where AM['english_word']['foreign_word'] 
-	is the computed expectation that the foreign_word is produced by english_word.
-	
-			LM['house']['maison'] = 0.5
+    max_iter : 		(int) the maximum number of iterations of the EM algorithm
+    fn_AM : 		(string) the location to save the alignment model
+
+    OUTPUT:
+    AM :			(dictionary) alignment model structure
+
+    The dictionary AM is a dictionary of dictionaries where AM['english_word']['foreign_word'] 
+    is the computed expectation that the foreign_word is produced by english_word.
+
+            LM['house']['maison'] = 0.5
     """
     AM = {}
-    
+
     # Read training data
     eng, fre = read_hansard(train_dir, num_sentences)
         
     # Initialize AM uniformly
     t = initalize(eng,fre)
-    
+
     # Iterate between E and M steps
     for _ in range(max_iters):
         t = em_step(t,eng,fre)
-    
+
     AM = t
     with open(fn_AM+'.pickle', 'wb') as handle:
         pickle.dump(AM, handle, protocol=pickle.HIGHEST_PROTOCOL)
@@ -44,19 +44,19 @@ def align_ibm1(train_dir, num_sentences, max_iter, fn_AM):
 # ------------ Support functions --------------
 def read_hansard(train_dir, num_sentences):
     """
-	Read up to num_sentences from train_dir.
-	
-	INPUTS:
-	train_dir : 	(string) The top-level directory name containing data
-					e.g., '/u/cs401/A2_SMT/data/Hansard/Testing/'
-	num_sentences : (int) the maximum number of training sentences to consider
-	
-	
-	Make sure to preprocess!
-	Remember that the i^th line in fubar.e corresponds to the i^th line in fubar.f.
-	
-	Make sure to read the files in an aligned manner.
-	"""
+    Read up to num_sentences from train_dir.
+
+    INPUTS:
+    train_dir : 	(string) The top-level directory name containing data
+                e.g., '/u/cs401/A2_SMT/data/Hansard/Testing/'
+    num_sentences : (int) the maximum number of training sentences to consider
+
+
+    Make sure to preprocess!
+    Remember that the i^th line in fubar.e corresponds to the i^th line in fubar.f.
+
+    Make sure to read the files in an aligned manner.
+    """
 
     sentCounter = 0
 
@@ -91,10 +91,10 @@ def read_hansard(train_dir, num_sentences):
 
 def initialize(eng, fre):
     """
-	Initialize alignment model uniformly.
-	Only set non-zero probabilities where word pairs appear in corresponding sentences.
-	"""
-	AM = {}
+    Initialize alignment model uniformly.
+    Only set non-zero probabilities where word pairs appear in corresponding sentences.
+    """
+    AM = {}
 
     for i in range(len(eng)):
         for e_word in eng[i].split(' '):
