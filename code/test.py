1c1555f08c23c49813b5a3bb303aef54eeef941d
mphamhung <hungmic2@teach.cs.toronto.edu>
2019-03-08 16:10:13 -0500

    made txt

--- code/test.py
+++ code/test.py
@@ -30,7 +30,7 @@ if os.path.getsize(os.getcwd()+'/ftest.pickle') > 0:
         lm2 = unpickler.load()
 
 
-log_prob(s, lm1, smoothing = True, delta = 0.1, vocabSize = 400)
+#log_prob(s, lm1, smoothing = True, delta = 0.1, vocabSize = 400)
 
 f = open("Task3.txt", "w+")
 
@@ -44,18 +44,31 @@ f.write(discussion)
 
 test_dir = '/u/cs401/A2_SMT/data/Hansard/Testing/'
 f.write('English Perplexity Testing')
+f.write('\n\n')
 f.write('No delta: ' + str(preplexity(lm1, test_dir, 'e', smoothing = False, delta = 0)))
+f.write('\n')
 f.write('delta 0.1: ' + str(preplexity(lm1, test_dir, 'e', smoothing = True, delta = 0.1)))
+f.write('\n')
 f.write('delta 0.25: ' + str(preplexity(lm1, test_dir, 'e', smoothing = True, delta = 0.25)))
+f.write('\n')
 f.write('delta 0.5: ' + str(preplexity(lm1, test_dir, 'e', smoothing = True, delta = 0.5)))
+f.write('\n')
 f.write('delta 0.75: ' + str(preplexity(lm1, test_dir, 'e', smoothing = True, delta = 0.75)))
-
+f.write('\n')
+f.write('\n')
 f.write('French Perplexity Testing')
+f.write('\n')
+f.write('\n')
 f.write('No delta: ' + str(preplexity(lm2, test_dir, 'f', smoothing = False, delta = 0)))
+f.write('\n')
 f.write('delta 0.1: ' + str(preplexity(lm2, test_dir, 'f', smoothing = True, delta = 0.1)))
+f.write('\n')
 f.write('delta 0.25: ' + str(preplexity(lm2, test_dir, 'f', smoothing = True, delta = 0.25)))
+f.write('\n')
 f.write('delta 0.5: ' + str(preplexity(lm2, test_dir, 'f', smoothing = True, delta = 0.5)))
+f.write('\n')
 f.write('delta 0.75: ' + str(preplexity(lm1, test_dir, 'e', smoothing = True, delta = 0.75)))
+f.write('\n')
 
 #print('-----Testing English Perplexity-----')
 #print('No delta: ', preplexity(lm1, test_dir, 'e', False, 0))
